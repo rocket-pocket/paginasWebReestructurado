58a2b60dffe0363d18f7d5b203584c6c10fe3025
rocket-pocket <verdudaniel7@gmail.com>
2018-07-20 21:06:16 +0200

    fizbuzz

---
+++ Ejercicios/01_Comienzo.js
@@ -0,0 +1,17 @@
+//FizzBuzz
+
+let contador = 1
+while(contador <= 100){ 
+    if (contador % 3 == 0) {
+        console.log('Fizz')
+    }else if (contador % 5 == 0) {
+        console.log('buzz')
+    } else {
+        console.log(contador)
+    contador = contador +1
+    }
+}  
+    
+
+
+
